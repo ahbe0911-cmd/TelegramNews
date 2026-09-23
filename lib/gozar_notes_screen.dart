f230a6261ab124e1b3029655f298d5b9f95f49ba
ahbe0911-cmd <ahbe0911@gmail.com>
2026-09-23 09:14:45 +0330

    fix(gozar): import ValueListenable explicitly for today-notes dashboard widget

--- lib/gozar_notes_screen.dart
+++ lib/gozar_notes_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shamsi_date/shamsi_date.dart';
